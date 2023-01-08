c9d61f5a8e90067bd0e28c40058546e468769ec3
Sagar D <sagar.0817.d@gmail.com>
2023-01-08 17:54:10 +0530

    Add query to get data of category retention for same customers

---
+++ models/category_retention_data/customers_category_retention.sql
@@ -0,0 +1,30 @@
+WITH all_data_combined AS (
+  SELECT 
+  customers.customer_unique_id, 
+  orders.order_purchase_timestamp,
+  orders.order_id,
+  product_category.product_category_name_english as product_category_name
+  FROM {{ ref('olist_customers_dataset') }} as customers
+  LEFT JOIN {{ ref('olist_orders_dataset') }} as orders
+  USING(customer_id)
+  LEFT JOIN {{ ref('olist_order_items_dataset') }} as order_items
+  USING(order_id)
+  LEFT JOIN {{ ref('olist_products_dataset') }} as products
+  USING(product_id)
+  LEFT JOIN {{ ref('product_category_name_translation') }} as product_category
+  USING(product_category_name)
+)
+
+SELECT 
+DISTINCT
+a1.customer_unique_id,
+a1.product_category_name,
+a1.order_purchase_timestamp,
+a1.order_id,
+a2.order_purchase_timestamp as order_purchase_timestamp_prev,
+a2.order_id as order_id_prev,
+FROM all_data_combined as a1
+LEFT JOIN all_data_combined as a2
+-- previous order purchased by the same customer in the same product category 
+ON a1.customer_unique_id = a2.customer_unique_id AND a1.order_purchase_timestamp > a2.order_purchase_timestamp AND a1.product_category_name = a2.product_category_name
+WHERE a2.order_purchase_timestamp IS NOT NULL
